0f45027b0087d4ac2da76b5e6e5077e35e6610e5
kanato1221 <kanatonton2@gmail.com>
2026-07-19 13:58:40 +0900

    Initial Commit

---
+++ short_diary/short_diaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  short_diaryApp.swift
+//  short_diary
+//
+//  Created by 松井奏人 on 2026/07/19.
+//
+
+import SwiftUI
+
+@main
+struct short_diaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
